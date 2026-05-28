fb66b012014a4140fe2b4799777f7dcda84bfe14
Bornface-CCT <sba25370@student.cct.ie>
2026-05-29 00:21:35 +0100

    Dashboard creation

---
+++ CA2 Dashboard.py
@@ -0,0 +1,68 @@
+# Importing libraries
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Dashboard Title
+st.title("Book Recommendation Dashboard")
+st.subheader("Dashboard Designed for Adults Senior Citizens")
+
+# Large Font Styling
+st.markdown(
+    """
+    <style>
+    html, body, [class*="css"]  {
+        font-size: 22px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Top Genres
+st.header("Top categories")
+
+Top_categories = books2['categories'].value_counts().head(10)
+
+fig1 = px.bar(
+    x=Top_categories.index,
+    y=Top_categories.values,
+    labels={'x':'categories', 'y':'Count'}
+)
+
+st.plotly_chart(fig1)
+
+# Ratings Distribution
+st.header("Ratings Distribution")
+
+fig2 = px.histogram(
+    books2,
+    x='ratings_count',
+    nbins=20
+)
+
+st.plotly_chart(fig2)
+
+# Top Rated Books
+st.header("Top Rated Books")
+
+Top_Rated_Books = books2.groupby('title')['ratings_count'].mean().sort_values(ascending=False).head(10)
+
+fig3 = px.bar(
+    x=Top_Rated_Books.index,
+    y=Top_Rated_Books.values
+)
+
+st.plotly_chart(fig3)
+
+# Active Users
+st.header("Most Books by Authors")
+
+Most_Books_by_Authors = books2['authors'].value_counts().head(10)
+
+fig4 = px.bar(
+    x=Most_Books_by_Authors.index.astype(str),
+    y=Most_Books_by_Authors.values
+)
+
+st.plotly_chart(fig4)
